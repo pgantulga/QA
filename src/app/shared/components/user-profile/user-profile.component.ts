b2e71bed2c2655b89fe7eebe415d6241856f5a9c
Tulgaa <pggan2@gmail.com>
2020-08-04 10:45:22 +0800

    user-profile-style

--- src/app/shared/components/user-profile/user-profile.component.ts
+++ src/app/shared/components/user-profile/user-profile.component.ts
@@ -3,7 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent {
   @Input() user: any;
